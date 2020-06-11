75de3b11c4a33143c4e585dbf8c552f791d5762d
EPNX0 <64423329+EPNX0@users.noreply.github.com>
2020-06-11 19:50:00 +0200

    All recent updates, new files, images etc.; Quite a lot added in recent time

--- Paper ODE/GridSearch.py
+++ Paper ODE/GridSearch.py
@@ -13,12 +13,13 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 import random
-random.seed(1)
+seed = 1
+random.seed(seed)  # 
 np.random.seed(1)
 try:
     dic
 except NameError:
-    dic = {' ':['beta', 'sigma', 'gamma', 'mu', 'E0', 'L']}
+    dic = {f'seed={seed}':['beta', 'sigma', 'gamma', 'mu', 'E0', 'L']}
 
 '''time grid
 
@@ -128,8 +129,8 @@ DF.to_csv('./USResultsGridSearch.csv',index=False)
 '''Test for the Germany
 '''
 PATH = 'C:/Users/Erich/Desktop/Theoretische Biophysik; Systembiologie/FK/Fachkurs_ODE/Paper ODE'
-Infectious = list(pd.read_csv(os.path.join(PATH, 'Germany_infectious.csv')).iloc[:,-1])
-Removed = list(pd.read_csv(os.path.join(PATH, 'Germany_removed.csv')).iloc[:, -1])
+Infectious = list(pd.read_csv(os.path.join(PATH, 'Germany/Germany_infectious.csv')).iloc[:,-1])
+Removed = list(pd.read_csv(os.path.join(PATH, 'Germany/Germany_removed.csv')).iloc[:, -1])
 Population = pd.read_csv(os.path.join(PATH, 'Total_Populations.csv'))['Germany'][0]
 
 # time span
@@ -144,24 +145,93 @@ R_true = Removed[:-7]
 ground_truth = [I_true, R_true]
 
 # Initial Values
-for frac in [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]:
+for frac in [5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]:
+    for i in range(10):
+        print(frac)
+        I0 = Infectious[0]  # Infectious Individuals at t=0; who are infected and can transmit it (confirmed)
+        R0 = Removed[0]  # Removed individuals at t=0; who died+recovered
+        N0 = Population  # Some Population < Total population because of stay-at-home order
+        E0 = int((frac * I0) + 0.5)  # Exposed Individuals at t=0; infected but not tested
+        S0 = N0 - E0 - I0 - R0  # Susceptible Individuals at t=0; Those who can be infected
+        init_values = [S0, E0, I0, R0]  # Values from Data at start-time
+        
+        # Parameters ti tune
+        random_pars = np.arange(0, 1, 0.1)
+        beta = random.choice(random_pars)  # Contact rate between S and (E + I)
+        sigma = random.choice(random_pars)  # Frac of E that either are I or R without confirmation
+        gamma = random.choice(random_pars)  # Transition rate between I and R
+        mu = random.choice(random_pars)  # Discovery rate of I
+        bounds = ((0, 1), (0, 1), (0, 1), (0, 1))  # boundaries for parameters
+        parameters = {'beta': beta, 'sigma': sigma, 'gamma': gamma, 'mu':mu}
+        #print([par for par in parameters.values()])
+        
+        res = Argmin([par for par in parameters.values()], time_span, init_values, 
+                     ground_truth, wanted_times, bounds)
+        print(res)
+        
+        if res.success==True:
+            # Validation
+            # Initial Values
+            I0 = Infectious[0]  # Infectious Individuals at t=0; who are infected and can transmit it (confirmed)
+            R0 = Removed[0]  # Removed individuals at t=0; who died+recovered
+            N0 = Population  # Some Population < Total population because of stay-at-home order
+            E0 = int((frac * I0) + 0.5)  # Exposed Individuals at t=0; infected but not tested
+            S0 = N0 - E0 - I0 - R0  # Susceptible Individuals at t=0; Those who can be infected
+            init_values = [S0, E0, I0, R0]  # Values from Data at start-time
+            # ground truth
+            I_true = Infectious[-7:]
+            R_true = Removed[-7:]
+            ground_truth = [I_true, R_true]
+            # time span
+            start = 0
+            end = len(Removed)
+            wanted_times = list(range(end-7, end))  # for t_eval, result for each day
+            time_span = (start, end)
+            # parameters taken from res if successful
+            L = LMSE(res.x.tolist(), time_span, init_values, ground_truth, wanted_times)
+            dic.update({'Germany'+str(frac)+'_'+str(i): [res.x[0], res.x[1], res.x[2], res.x[3], f'{frac} * {I0}', L]})
+DF = pd.DataFrame(dic)
+DF.to_csv('./Germany/GermanyGridSearch6.csv',index=False)
+
+
+'''Test for the TBPH
+
+repeats = 80
+PATH = 'C:/Users/Erich/Desktop/Theoretische Biophysik; Systembiologie/FK/Fachkurs_ODE/Paper ODE/TBPH'
+Infectious = list(pd.read_csv(os.path.join(PATH, 'Infectious_TBPH.csv')).iloc[:,-1])[72:408]
+Removed = list(pd.read_csv(os.path.join(PATH, 'Removed_TBPH.csv')).iloc[:, -1])[72:408]
+
+# time span
+start = 0
+end = len(Removed)
+wanted_times = list(range(start, end-7))  # for t_eval, result for each day
+time_span = (start, end)
+
+# Reported Values
+I_true = Infectious[:-7]
+R_true = Removed[:-7]
+ground_truth = [I_true, R_true]
+
+# Initial Values
+for i in range(repeats):
     I0 = Infectious[0]  # Infectious Individuals at t=0; who are infected and can transmit it (confirmed)
     R0 = Removed[0]  # Removed individuals at t=0; who died+recovered
-    N0 = Population  # Some Population < Total population because of stay-at-home order
-    E0 = int((frac * N0) + 0.5)  # Exposed Individuals at t=0; infected but not tested
+    N0 = 10290  # Some Population < Total population because of stay-at-home order
+    E0 = 1004  # Exposed Individuals at t=0; infected but not tested
     S0 = N0 - E0 - I0 - R0  # Susceptible Individuals at t=0; Those who can be infected
     init_values = [S0, E0, I0, R0]  # Values from Data at start-time
     
     # Parameters ti tune
-    random_pars = np.array([0., 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.])
+    random_pars = np.arange(0,1.01,0.01)
     beta = random.choice(random_pars)  # Contact rate between S and (E + I)
     sigma = random.choice(random_pars)  # Frac of E that either are I or R without confirmation
     gamma = random.choice(random_pars)  # Transition rate between I and R
     mu = random.choice(random_pars)  # Discovery rate of I
     bounds = ((0, 1), (0, 1), (0, 1), (0, 1))  # boundaries for parameters
     parameters = {'beta': beta, 'sigma': sigma, 'gamma': gamma, 'mu':mu}
+    #print([par for par in parameters.values()])
     
-    res = Argmin([param for param in parameters.values()], time_span, init_values, 
+    res = Argmin([par for par in parameters.values()], time_span, init_values, 
                  ground_truth, wanted_times, bounds)
     print(res)
     
@@ -170,8 +240,8 @@ for frac in [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]:
         # Initial Values
         I0 = Infectious[0]  # Infectious Individuals at t=0; who are infected and can transmit it (confirmed)
         R0 = Removed[0]  # Removed individuals at t=0; who died+recovered
-        N0 = Population  # Some Population < Total population because of stay-at-home order
-        E0 = int((frac * N0) + 0.5)  # Exposed Individuals at t=0; infected but not tested
+        N0 = 10290  # Some Population < Total population because of stay-at-home order
+        E0 = 1004  # Exposed Individuals at t=0; infected but not tested
         S0 = N0 - E0 - I0 - R0  # Susceptible Individuals at t=0; Those who can be infected
         init_values = [S0, E0, I0, R0]  # Values from Data at start-time
         # ground truth
@@ -185,9 +255,7 @@ for frac in [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]:
         time_span = (start, end)
         # parameters taken from res if successful
         L = LMSE(res.x.tolist(), time_span, init_values, ground_truth, wanted_times)
-        dic.update({'US'+str(frac): [res.x[0], res.x[1], res.x[2], res.x[3], f'{frac} * {N0}', L]})
+        dic.update({'TBPH'+str(i): [res.x[0], res.x[1], res.x[2], res.x[3], E0, L]})
 DF = pd.DataFrame(dic)
-DF.to_csv('./GermanyResultsGridSearch.csv',index=False)
-
-
-
+DF.to_csv(os.path.join(PATH, 'TBPHGridSearchOliver2.csv'), index=False)
+'''
---
+++ Paper ODE/UsingODEModel.py
@@ -0,0 +1,64 @@
+"""
+Created on Thu Jun 11 11:58:41 2020
+
+@author: Erich
+"""
+import sys
+import os
+sys.path.append('C:/Users/Erich/Desktop/Theoretische Biophysik; Systembiologie/FK/Fachkurs_ODE/Paper ODE')
+import numpy as np
+import pandas as pd
+import math
+import matplotlib.pyplot as plt
+from LearningTheta import SuEIR
+from scipy.integrate import solve_ivp
+
+Infectious = list(pd.read_csv('./Germany/Germany_infectious.csv').iloc[:, -1])[:]
+Removed = list(pd.read_csv('./Germany/Germany_removed.csv').iloc[:, -1])[:]
+Population = pd.read_csv('./Total_Populations.csv')['Germany']
+Parameters = pd.read_csv('./Germany/GermanyGridSearch6.csv')
+Keys = Parameters.keys()
+
+'''Time span
+'''
+start = 0
+end = len(Removed)+50
+t_span = (start, end)
+t_eval = list(range(start, end))
+
+for i in range(1, len(Keys)):
+    '''Initial Values
+    '''
+    I0 = Infectious[0]
+    R0 = Removed[0]
+    N0 = Population
+    E0 = int((int(Parameters[Keys[i]][4][0]) * I0) + 0.5)
+    S0 = N0 - I0 - R0 - E0
+    Init_values = [S0, E0, I0, R0]
+    '''Parameters
+    '''
+    params = Parameters[Keys[i]]
+    beta = float(params[0])
+    sigma = float(params[1])
+    gamma = float(params[2])
+    mu = float(params[3])
+    parameters = [beta, sigma, gamma, mu]
+    
+    res = solve_ivp(SuEIR, t_span, Init_values, args=(parameters), t_eval=t_eval)
+    
+    X_true = np.arange(len(Infectious))
+    X_pred = res.t
+    I_true = Infectious
+    R_true = Removed
+    I_pred = res.y[-2]
+    R_pred = res.y[-1]
+    plt.plot(X_true, I_true, label='Infectious_true', color='blue')
+    plt.plot(X_true, R_true, label='Removed_true', color='violet')
+    plt.plot(X_pred, I_pred, label='Infectious_pred', linestyle='--', color='orange')
+    plt.plot(X_pred, R_pred, label='Removed_pred', linestyle='--', color='red')
+    plt.legend()
+    plt.xlabel('Days')
+    plt.ylabel('# Of Individuals')
+    plt.savefig(f'./Germany/Grid6/Grid{Keys[i]}.png', dpi=600, bbox_inches='tight')
+    plt.close()
+
